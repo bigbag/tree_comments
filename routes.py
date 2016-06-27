6c3c1691821b8d80c94cbf2d5fde481b0a37bf3b
Pavel Liashkov <bilbo.kem@gmail.com>
2016-06-27 16:23:00 +0300

    Added entity actions to routers

--- routes.py
+++ routes.py
@@ -1,14 +1,23 @@
+"""Main router"""
+
 from views.comment import CommentView
 from views.user import UserView
+from views.entity import EntityView
 
 comment_view = CommentView()
 user_view = UserView()
+entity_view = EntityView()
 
 routes = [
     ('GET', '/user/', user_view.list, 'user_list'),
     ('GET', '/user/{user_id}', user_view.get, 'user_details'),
     ('POST', '/user/', user_view.create, 'user_create'),
     ('DELETE', '/user/{user_id}', user_view.delete, 'user_delete'),
+
+    ('GET', '/entity/', entity_view.list, 'entity_list'),
+    ('GET', '/entity/{entity_id}', entity_view.get, 'entity_details'),
+    ('POST', '/entity/', entity_view.create, 'entity_create'),
+    ('DELETE', '/entity/{entity_id}', entity_view.delete, 'entity_delete'),
 
     ('GET', '/comment/', comment_view.list, 'comment_list'),
     ('GET', '/comment/{comment_id}', comment_view.get, 'comment_details'),
