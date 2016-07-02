52fcf0934409b55d6af884cde2258b3c1884306b
Pavel Liashkov <bilbo.kem@gmail.com>
2016-07-02 11:47:34 +0300

    Added migration for user table

---
+++ migrations/versions/954c8dfb878e_create_user_table.py
@@ -0,0 +1,30 @@
+"""Create user table
+
+Revision ID: 954c8dfb878e
+Revises: 
+Create Date: 2016-07-02 11:45:09.611147
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = '954c8dfb878e'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.create_table(
+        'user',
+        sa.Column('id', sa.BigInteger(), nullable=False),
+        sa.Column('name', sa.String(length=100), nullable=False),
+        sa.Column('create_date', sa.TIMESTAMP, nullable=False),
+        sa.PrimaryKeyConstraint('id')
+    )
+
+
+def downgrade():
+    op.drop_table('user')
