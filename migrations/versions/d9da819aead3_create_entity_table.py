38d60531b253f17df1218ca670bf08be487c4dae
Pavel Liashkov <bilbo.kem@gmail.com>
2016-07-02 11:49:37 +0300

    Added migration for entity table

---
+++ migrations/versions/d9da819aead3_create_entity_table.py
@@ -0,0 +1,31 @@
+"""Create entity table
+
+Revision ID: d9da819aead3
+Revises: 954c8dfb878e
+Create Date: 2016-07-02 11:48:00.742587
+
+"""
+
+# revision identifiers, used by Alembic.
+revision = 'd9da819aead3'
+down_revision = '954c8dfb878e'
+branch_labels = None
+depends_on = None
+
+from alembic import op
+import sqlalchemy as sa
+
+
+def upgrade():
+    op.create_table(
+        'entity',
+        sa.Column('id', sa.BigInteger, primary_key=True),
+        sa.Column('name', sa.String(100), nullable=False),
+        sa.Column('type', sa.String(100), nullable=False),
+        sa.Column('create_date', sa.TIMESTAMP, nullable=False),
+        sa.PrimaryKeyConstraint('id')
+    )
+
+
+def downgrade():
+    op.drop_table('entity')
